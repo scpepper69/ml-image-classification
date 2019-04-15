b2657f67f75100ff417828b5dc6949e79a2520ae
scpepper69 <apps@tsawada-learning.workers1.gotestcpvcn.oraclevcn.com>
2019-04-15 06:57:15 +0000

    batchnorm

---
+++ tensorflow/japanese/learning/learn_japanese_batchnorm.py
@@ -0,0 +1,205 @@
+import os
+os.environ["KERAS_BACKEND"] = "tensorflow"
+kerasBKED = os.environ["KERAS_BACKEND"] 
+print(kerasBKED)
+
+import shutil
+from pathlib import Path
+
+import numpy as np
+import scipy.misc
+
+import tensorflow as tf
+from tensorflow.python.framework import graph_util, mod_graph_util
+from tensorflow.python.framework import graph_io
+
+from tensorflow.python import keras
+from tensorflow.python.estimator.export import export
+
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras import initializers
+from tensorflow.python.keras.layers import Conv2D, Convolution2D, MaxPooling2D
+from tensorflow.python.keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras.utils import np_utils
+from sklearn.model_selection import train_test_split
+
+nb_classes = 956
+# input image dimensions
+img_rows, img_cols = 32, 32
+# img_rows, img_cols = 127, 128
+
+ary = np.load("japanese.npz")['arr_0'].reshape([-1, 127, 128]).astype(np.float32) / 15
+X_train = np.zeros([nb_classes * 160, img_rows, img_cols], dtype=np.float32)
+for i in range(nb_classes * 160):
+    X_train[i] = scipy.misc.imresize(ary[i], (img_rows, img_cols), mode='F')
+    # X_train[i] = ary[i]
+Y_train = np.repeat(np.arange(nb_classes), 160)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.2)
+
+X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(Y_train, nb_classes)
+Y_test = np_utils.to_categorical(Y_test, nb_classes)
+
+datagen = ImageDataGenerator(rotation_range=15, zoom_range=0.20)
+datagen.fit(X_train)
+
+model = Sequential()
+
+
+def my_init(shape, dtype=None):
+#    return K.random_normal(shape, dtype=dtype)
+    return initializers.TruncatedNormal(shape, scale=0.1, name=name)
+#    return initializations.normal(shape, scale=0.1, name=name)
+
+
+# Best val_loss: 0.0205 - val_acc: 0.9978 (just tried only once)
+# 30 minutes on Amazon EC2 g2.2xlarge (NVIDIA GRID K520)
+def m6_1():
+#    model.add(Conv2D(32, (3, 3), kernel_initializer=my_init, input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), kernel_initializer=initializers.TruncatedNormal(stddev=0.1), input_shape=input_shape))
+    model.add(Activation('relu'))
+#    model.add(Conv2D(32, (3, 3), kernel_initializer=initializers.TruncatedNormal(stddev=0.1)))
+#    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+#    model.add(Dropout(0.2))
+
+    model.add(Conv2D(64, (3, 3), kernel_initializer=initializers.TruncatedNormal(stddev=0.1)))
+    model.add(Activation('relu'))
+#    model.add(Conv2D(64, (3, 3), kernel_initializer=initializers.TruncatedNormal(stddev=0.1)))
+#    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+#    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(1024, kernel_initializer=initializers.TruncatedNormal(stddev=0.1)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+
+def classic_neural():
+    model.add(Flatten(input_shape=input_shape))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+
+
+m6_1()
+# classic_neural()
+
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+model.fit_generator(datagen.flow(X_train, Y_train, batch_size=16), steps_per_epoch=X_train.shape[0],epochs=40, validation_data=(X_test, Y_test))
+#epochs=400
+
+# additional
+score = model.evaluate(X_train, Y_train, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+import os
+import shutil
+from pathlib import Path
+
+K.set_learning_phase(0)
+ksess = K.get_session()
+#print(ksess)
+
+#K.set_learning_phase(0)
+graph = ksess.graph
+kgraph = graph.as_graph_def()
+print(kgraph)
+
+# define output node
+output_names = [node.op.name for node in model.outputs]
+freeze_var_names = list(set(v.op.name for v in tf.global_variables()))
+#num_output = 1
+#prefix = "output"
+#pred = [None]*num_output
+#outputName = [None]*num_output
+#for i in range(num_output):
+#    outputName[i] = prefix + str(i)
+#    pred[i] = tf.identity(model.get_output_at(i), name=outputName[i])
+print('output name: ', output_names)
+print('freeze_var_names:', freeze_var_names)
+
+# fix batch norm nodes
+for node in kgraph.node:
+    if node.op == 'RefSwitch':
+        node.op = 'Switch'
+        for index in range(len(node.input)):
+            if 'moving_' in node.input[index] and "Switch" not in node.input[index]:
+                node.input[index] = node.input[index] + '/read'
+    elif node.op == 'AssignSub':
+        node.op = 'Sub'
+        if 'use_locking' in node.attr: del node.attr['use_locking']
+    elif node.op == 'AssignAdd':
+        node.op = 'Add'
+        if 'use_locking' in node.attr: del node.attr['use_locking']
+
+# convert variables in the model graph to constants
+constant_graph = mod_graph_util.convert_variables_to_constants(ksess, kgraph, output_names)
+#constant_graph = graph_util.convert_variables_to_constants(ksess, kgraph, [i.name[:-2] for i in model.outputs])
+#constant_graph = tf.compat.v1.graph_util.convert_variables_to_constants(ksess, kgraph, [i.name[:-2] for i in model.outputs])
+
+
+output_dir = "./"
+output_graph_name = "japanese.pb"
+output_text_name = "japanese.txt"
+graph_io.write_graph(constant_graph, output_dir, output_graph_name, as_text=False)
+graph_io.write_graph(constant_graph, output_dir, output_text_name, as_text=True)
+print('saved graph .pb at: {0}\nsaved graph .txt at: {1}'.format(
+        os.path.join(output_dir, output_graph_name),
+        os.path.join(output_dir, output_text_name)))
+
+
+### after export pb by write_graph, read pf file again and export by savemodelbuilder
+OUTPUT_SERVABLE_FOLDER="./tf_japanese"
+if os.path.exists(OUTPUT_SERVABLE_FOLDER):
+    shutil.rmtree(OUTPUT_SERVABLE_FOLDER)
+
+from tensorflow.python.saved_model import signature_constants
+from tensorflow.python.saved_model import tag_constants
+
+builder = tf.saved_model.builder.SavedModelBuilder(OUTPUT_SERVABLE_FOLDER)
+
+with tf.gfile.GFile('./japanese.pb', "rb") as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+
+sigs = {}
+OUTPUT_TENSOR = output_names
+with tf.Session(graph=tf.Graph()) as sess:
+    tf.import_graph_def(graph_def, name="")
+    g = tf.get_default_graph()
+    inp = model.input #g.get_tensor_by_name(INPUT_TENSOR)
+    out = g.get_tensor_by_name(OUTPUT_TENSOR[0] + ':0')
+
+    sigs[signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY] = \
+        tf.saved_model.signature_def_utils.predict_signature_def(
+            {"input": inp}, {"outout": out})
+
+    builder.add_meta_graph_and_variables(sess,
+                                         [tag_constants.SERVING],
+                                         signature_def_map=sigs)
+    try:
+        builder.save()
+        print(f'Model ready for deployment at {OUTPUT_SERVABLE_FOLDER}/saved_model.pb')
+        print('Prediction signature : ')
+        print(sigs['serving_default'])
+    except:
+        print('Error Occured, please checked frozen graph')
+
