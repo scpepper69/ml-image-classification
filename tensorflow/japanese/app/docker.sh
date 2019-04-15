e19db62c3640aeed339bf4848e6ae4e2b77a9149
scpepper69 <apps@scp001-gp.us-west1-b.c.steadfast-tesla-224607.internal>
2019-04-15 07:30:52 +0000

    japanese app

---
+++ tensorflow/japanese/app/docker.sh
@@ -0,0 +1,1 @@
+docker run -it --name japanese --rm -v "/home/apps/git/ml/tensorflow/japanese/learning:/models/" -p 9012:9012 sleepsonthefloor/graphpipe-tf:cpu --model=/models/japanese.pb --listen=0.0.0.0:9012
